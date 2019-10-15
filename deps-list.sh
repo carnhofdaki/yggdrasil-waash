56a07ff6e0d27363330468f95bd62ee05f91d189
Carnhof Daki <carnhofdaki@gmail.com>
2019-10-15 11:55:25 +0200

    Bump yggdrasil to 0.3.10

--- deps-list.sh
+++ deps-list.sh
@@ -7,5 +7,5 @@ https://code.kliu.org/misc/elevate/elevate-1.3.0-redist.7z
 https://swupdate.openvpn.org/community/releases/tap-windows-9.9.2_3.exe
 http://unxutils.sourceforge.net/UnxUpdates.zip
 https://www.7-zip.org/a/7z1900.exe
-https://2023-115685026-gh.circle-artifacts.com/0/yggdrasil-0.3.9-windows-i386.exe
+https://2106-115685026-gh.circle-artifacts.com/0/yggdrasil-0.3.10-windows-i386.exe
 EOF
