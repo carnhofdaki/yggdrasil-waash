286690cbdf91b26e7fec4f31814d19efabab5f31
Daki Carnhof <carnhofdaki@gmail.com>
2019-11-27 09:29:27 +0100

    Change source of elevate-1.3.0-redist.7z
    The binary is the same.
    From https://code.kliu.org/misc/elevate/

--- deps-list.sh
+++ deps-list.sh
@@ -3,7 +3,7 @@
 . $PWD/vars.inc
 
 cat <<EOF
-https://code.kliu.org/misc/elevate/elevate-1.3.0-redist.7z
+https://github.com/carnhofdaki/yggdrasil-waash/releases/download/v0.3.12.0/elevate-1.3.0-redist.7z
 https://swupdate.openvpn.org/community/releases/tap-windows-9.9.2_3.exe
 http://unxutils.sourceforge.net/UnxUpdates.zip
 https://www.7-zip.org/a/7z1900.exe
