8060aad1b906da4715e8cd18b53b7f7a10c89324
Daki Carnhof <carnhofdaki@gmail.com>
2020-10-18 11:41:46 +0200

    Update yggdrasil to 0.3.15

--- deps-list.sh
+++ deps-list.sh
@@ -7,5 +7,5 @@ https://github.com/carnhofdaki/yggdrasil-waash/releases/download/v0.3.12.0/eleva
 https://swupdate.openvpn.org/community/releases/tap-windows-9.9.2_3.exe
 http://unxutils.sourceforge.net/UnxUpdates.zip
 https://www.7-zip.org/a/7z1900.exe
-https://2375-115685026-gh.circle-artifacts.com/0/yggdrasil-0.3.12-windows-i386.exe
+https://3636-115685026-gh.circle-artifacts.com/0/yggdrasil-0.3.15-windows-i386.exe
 EOF
