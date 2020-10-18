44fd2328e7dd32b5099c03dc3681a8718b34c7e1
Daki Carnhof <carnhofdaki@gmail.com>
2020-10-18 12:45:08 +0200

    Use tmp for temporary files

--- 00-prepare.sh
+++ 00-prepare.sh
@@ -8,27 +8,23 @@ do type $dep; done
 test -r .prepared-$version || {
 . $PWD/vars.inc
 
-sh deps-list.sh | wget -nc -ci - || true
+test -d tmp || mkdir tmp
+sh deps-list.sh | (cd tmp; wget -nc -ci -) || true
 
-test -r elevate.exe || {
-  7z x elevate-1.3.0-redist.7z bin.x86-32/elevate.exe
-  mv bin.x86-32/elevate.exe ./
+test -r tmp/elevate.exe || {
+  7z x tmp/elevate-1.3.0-redist.7z bin.x86-32/elevate.exe
+  mv bin.x86-32/elevate.exe tmp
   rmdir bin.x86-32
 }
 md5sum -c <<EOF
-7178d69ded53b7683dd52cd1ca0a20ff  elevate.exe
+7178d69ded53b7683dd52cd1ca0a20ff  tmp/elevate.exe
 EOF
 
-test -r 7zCon.sfx || {
-  mkdir tmp
-  cd tmp
-  7z x ../7z1900.exe
-  mv 7zCon.sfx ..
-  cd ..
-  rm -rf tmp
+test -r tmp/7zCon.sfx || {
+  (cd tmp; 7z x 7z1900.exe)
 }
 
-upx yggdrasil-$version-windows-i386.exe
+upx tmp/yggdrasil-$version-windows-i386.exe
 
 touch .prepared-$version
 }
--- 01-release.sh
+++ 01-release.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -e
 
-test -r .prepared
-
 . $PWD/vars.inc
+test -r .prepared-$version
 
 OUT=yggdrasil-waash-${version}-i386-r${revision}
 R=waash/release
@@ -13,18 +12,18 @@ mkdir -p $R
 cp -r waash/data $ROUT
 mkdir -p $ROUT/deps/bin
 
-unzip UnxUpdates.zip sed.exe
+(cd tmp; unzip UnxUpdates.zip sed.exe)
 
 sh gen-README.sh > $ROUT/README.txt
-cp yggdrasil-$version-*.exe $ROUT/deps/bin/yggdrasil-windows-i386.exe
-cp *.exe $ROUT/deps/bin
-cp *.sfx $R
+cp tmp/yggdrasil-$version-*.exe $ROUT/deps/bin/yggdrasil-windows-i386.exe
+cp tmp/tap-*.exe $ROUT/deps/bin
+cp tmp/elevate.exe $ROUT/deps/bin
+mv tmp/sed.exe $ROUT/deps/bin
 unix2dos $ROUT/*.bat
 unix2dos $ROUT/deps/*.txt
 }
-cd $R
-7z a ${OUT}.7z $OUT
+(cd $R; 7z a ${OUT}.7z $OUT)
 
 # Make the self-extracting exe file
-cat *.sfx > ${OUT}.exe
-cat ${OUT}.7z >> ${OUT}.exe
+cat tmp/7zCon.sfx > $R/${OUT}.exe
+cat $R/${OUT}.7z >> $R/${OUT}.exe
